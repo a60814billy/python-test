97b22a3201d422b8fd1f84f1518c5765eb19af35
Raccoon Li <raccoon@hackmd.io>
2024-07-15 18:39:50 +0900

    change test case name on main

--- test_sum.py
+++ test_sum.py
@@ -3,7 +3,7 @@ import unittest
 
 import my_tool
 
-class TestSum(unittest.TestCase):
+class TestSumMain(unittest.TestCase):
     def test_sum1(self):
         result = my_tool.sum(10, 20)
         self.assertEqual(result, 30)
