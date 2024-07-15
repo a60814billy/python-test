463806c742ba40aca05efab38930d5fd1a2e0a1d
Raccoon Li <raccoon@hackmd.io>
2024-07-15 18:27:59 +0900

    init

---
+++ test_sum.py
@@ -0,0 +1,13 @@
+
+import unittest
+
+import my_tool
+
+class TestSum(unittest.TestCase):
+    def test_sum1(self):
+        result = my_tool.sum(10, 20)
+        self.assertEqual(result, 30)
+        
+    def test_sum2(self):
+        result = my_tool.sum(0, 0)
+        self.assertEqual(result,0)
